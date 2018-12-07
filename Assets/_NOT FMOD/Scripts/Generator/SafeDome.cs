9e1d08807a4d6f9d5e4bc178b7323138b2a93c3e
Alessandro <alessandro.fama@ymail.com>
2018-12-08 00:48:16 +0100

    FMOD integration installation, moved folders to _NOT FMOD for clarity

---
+++ Assets/_NOT FMOD/Scripts/Generator/SafeDome.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafeDome : MonoBehaviour
+{
+    private Player m_playerInside;
+
+    void Update()
+    {
+        if (m_playerInside != null && m_playerInside.CurrentSafeZone != this)
+        {
+            m_playerInside.CurrentSafeZone = this;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag != "Player") return;
+
+        m_playerInside = collision.GetComponent<Player>();
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag != "Player") return;
+
+        m_playerInside = null;
+    }
+}
---
+++ Assets/_NOT FMOD/Scripts/Scenes/Outro.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Outro : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        MusicPlayer.Instance.StartIntro();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
